80407dc948794bbaa3086b71e116c9cb10e73e2e
zhangxinling2 <2350486903@qq.com>
2025-06-13 17:53:43 +0800

    article publish test

--- Makefile
+++ Makefile
@@ -7,6 +7,9 @@ mock:
 	@mockgen -source=webook/internal/repository/code.go -package=svcmock -destination=webook/internal/repository/mocks/code.mock.go
 	@mockgen -source=webook/internal/repository/cache/user.go -package=cachemock -destination=webook/internal/repository/cache/mocks/user.mock.go
 	@mockgen -source=webook/internal/repository/dao/user.go -package=daomock -destination=webook/internal/repository/dao/mocks/user.mock.go
+    @mockgen -source=webook/internal/repository/article/author.go -package=articlemock -destination=webook/internal/repository/article/mocks/article/author.mock.go
+    @mockgen -source=webook/internal/repository/article/reader.go -package=articlemock -destination=webook/internal/repository/article/mocks/article/reader.mock.go
+
 	@mockgen -package=redismock -destination=webook/internal/repository/cache/redismocks/cmd.mock.go github.com/redis/go-redis/v9 Cmdable
 	@mockgen -source=webook/internal/repository/code.go -package=svcmock -destination=webook/internal/repository/mocks/code.mock.go
 	@go mod tidy
--- webook/internal/service/article/article_test.go
+++ webook/internal/service/article/article_test.go
@@ -1,7 +1,64 @@
 package article
 
-import "testing"
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+	"testing"
+	"webookProgram/webook/internal/domain"
+	"webookProgram/webook/internal/repository/article"
+	articlemock "webookProgram/webook/internal/repository/article/mocks/article"
+	"webookProgram/webook/pkg/logger"
+)
 
-func TestArticle(t *testing.T) {
+func Test_articleServiceV1_Publish(t *testing.T) {
+	testCases := []struct {
+		name    string
+		mock    func(ctrl *gomock.Controller) (article.ArticleAuthorRepository, article.ArticleReaderRepository)
+		art     domain.Article
+		wantId  int64
+		wantErr error
+	}{
+		{
+			name: "新建发表成功",
+			mock: func(ctrl *gomock.Controller) (article.ArticleAuthorRepository, article.ArticleReaderRepository) {
+				author := articlemock.NewMockArticleAuthorRepository(ctrl)
+				reader := articlemock.NewMockArticleReaderRepository(ctrl)
+				ctx := context.Background()
+				author.EXPECT().Create(ctx, domain.Article{
+					Title:   "新建发表",
+					Content: "新建发表",
+					Author: domain.Author{
+						Id:   123,
+						Name: "123",
+					},
+				}).Return(int64(1), nil)
+				reader.EXPECT().Save(ctx, domain.Article{
+					Id:      1,
+					Title:   "新建发表",
+					Content: "新建发表",
+					Author: domain.Author{
+						Id:   123,
+						Name: "123",
+					},
+				}).Return(nil)
+				return author, reader
+			},
+			wantId:  1,
+			wantErr: nil,
+		},
+	}
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			author, reader := tt.mock(ctrl)
+			h := NewArticleServiceV1(author, reader, logger.NewNoOpLogger())
+			id, err := h.Publish(context.Background(), tt.art)
+			assert.Equal(t, id, tt.wantId)
+			require.NoError(t, err)
 
+		})
+	}
 }
